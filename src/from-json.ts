18b7964974887c1dab7ae96ab10e82eaa22f769d
dikac <rigel234@yahoo.com>
2019-11-09 00:33:19 +0700

    allow toString for from-json

--- src/from-json.ts
+++ src/from-json.ts
@@ -1,9 +1,9 @@
 export default function FromJson<Type>(
-    json : string,
+    json : {toString:()=>string},
     validator : (object : any) => object is Type
 ) : Type {
 
-    let object = JSON.parse(json);
+    let object = JSON.parse(json.toString());
 
     if(validator(object)) {
 
