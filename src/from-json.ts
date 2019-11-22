5dea910b27ad5d38a7d642232303a293a9ef6d36
dikac <rigel234@yahoo.com>
2019-11-23 00:22:07 +0700

    add error callback to from-json

--- src/from-json.ts
+++ src/from-json.ts
@@ -1,14 +1,16 @@
 export default function FromJson<Type>(
     json : {toString:()=>string},
-    validator : (object : any) => object is Type
+    validator : (object : any) => object is Type,
+    error : (json : string, object : object) => Error = (json : string, object : object) => new TypeError('json string is not valid according to validator')
 ) : Type {
 
-    let object = JSON.parse(json.toString());
+    let string = json.toString();
+    let object = JSON.parse(string);
 
     if(validator(object)) {
 
         return object;
     }
 
-    throw new TypeError('json string is not valid according to validator');
+    throw error(string, object);
 }
