22cce78ed552228802870e029e65cbec20e8c8db
dikac <rigel234@yahoo.com>
2019-11-26 04:08:48 +0700

    add strict assign

---
+++ src/strictAssign.ts
@@ -0,0 +1,12 @@
+/**
+ * Strict assign
+ *
+ * @param object
+ * @param data
+ * @constructor
+ */
+export default function StrictAssign<O extends object, U extends {[Key in keyof O]}>(object : O, data : U) : U & O {
+
+    return Object.assign(object, data);
+}
+
