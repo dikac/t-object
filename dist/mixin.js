92e5fe7ddfc627ceae834c5ad280fbdde6f982cb
dikac <rigel234@yahoo.com>
2020-01-18 02:55:07 +0700

    mixin test

--- dist/mixin.js
+++ dist/mixin.js
@@ -12,9 +12,144 @@
     const descriptor_getter_1 = require("./boolean/descriptor-getter");
     const merge_descriptor_getter_setter_1 = require("./merge-descriptor-getter-setter");
     const descriptor_setter_1 = require("./boolean/descriptor-setter");
+    it("force console log", () => { spyOn(console, 'log').and.callThrough(); });
+    //
+    // class Handler implements ProxyHandler<Array<object>> {
+    //
+    //     /**
+    //      * A trap for the in operator.
+    //      * @param target
+    //      * @param p
+    //      */
+    //     has(target: Array<object>, p: string | number | symbol): boolean {
+    //
+    //         for(let object of target) {
+    //
+    //             if(p in object) {
+    //
+    //                 return true;
+    //             }
+    //         }
+    //
+    //         return false;
+    //
+    //     }
+    //
+    //     get(target: Array<object>, p: string | number | symbol, receiver: any): any {
+    //
+    //         for(let object of target) {
+    //
+    //             if(p in object) {
+    //
+    //                 if(Object.getOwnPropertyDescriptor(object, p)) {
+    //
+    //                     return object[p];
+    //                 }
+    //
+    //                 let descriptor = Object.getOwnPropertyDescriptor(Object.getPrototypeOf(object), p);
+    //
+    //                 if(descriptor && descriptor.get) {
+    //
+    //                     return object[p];
+    //                 }
+    //
+    //                 if(descriptor && descriptor.value) {
+    //
+    //                     return object[p];
+    //                 }
+    //             }
+    //         }
+    //
+    //         return undefined;
+    //
+    //     }
+    //
+    //     set(target: Array<object>, p: string | number | symbol, value: any, receiver: any): boolean {
+    //
+    //         for(let object of target) {
+    //
+    //             if(p in object) {
+    //
+    //                 if(Object.getOwnPropertyDescriptor(object, p)) {
+    //
+    //                     object[p] = value;
+    //                     return true;
+    //                 }
+    //
+    //                 let descriptor = Object.getOwnPropertyDescriptor(Object.getPrototypeOf(object), p);
+    //
+    //                 if(descriptor && descriptor.set) {
+    //
+    //                     object[p] = value;
+    //                     return true;
+    //                 }
+    //             }
+    //         }
+    //
+    //         return false;
+    //     }
+    //
+    //     apply(target: Array<object>, thisArg: any, argArray?: any): any {
+    //
+    //         console.log('thisArg =====================================');
+    //         console.log(thisArg);
+    //
+    //         return false;
+    //     }
+    //
+    //     deleteProperty(target: Array<object>, p: string | number | symbol): boolean {
+    //
+    //         for(let object of target) {
+    //
+    //             if(p in object) {
+    //
+    //                 delete object[p];
+    //                 return true;
+    //             }
+    //         }
+    //
+    //         return false;
+    //     }
+    // }
+    //
+    //
+    //
+    // export default function Mixin<Objects extends object[]>(...objects : Objects) : UnionToIntersection<ValuesType<Objects>> {
+    //
+    //
+    //     class ccc {
+    //         a  = 'a';
+    //         get b() : any { return null}
+    //         set c(c) { }
+    //         d(c) { }
+    //     }
+    //
+    //     // console.log('========>>');
+    //     // console.log(Object.getOwnPropertyDescriptor((new ccc()), 'a'));
+    //     // console.log(Object.getOwnPropertyDescriptor(Object.getPrototypeOf(new ccc()), 'b'));
+    //     // console.log(Object.getOwnPropertyDescriptor(Object.getPrototypeOf(new ccc()), 'c'));
+    //     // console.log(Object.getOwnPropertyDescriptor(Object.getPrototypeOf(new ccc()), 'd'));
+    //     // console.log('========<<');
+    //
+    //     let proxy = new Proxy(objects, new Handler());
+    //     return <UnionToIntersection<ValuesType<Objects>>>proxy;
+    //
+    // }
+    //
+    // class ccc {
+    //     a  = 'a';
+    //     get b() : any { return null}
+    //     set c(c) { }
+    //     d(c) { this.a = c }
+    // }
+    //
+    // let c = new ccc;
+    // // @ts-ignore
+    // console.log(Mixin(c).d(2));
+    // console.log(c.a);
+    //
     function Mixin(...objects) {
         // assign value
-        let target = Object.assign({}, ...objects);
         // populate descriptor
         let descriptors = new Map();
         for (let object of objects) {
@@ -36,11 +171,26 @@
                 }
             }
         }
+        let target = function () {
+        };
+        //let target = Object.assign(function () {}, ...objects);
+        // // assign descriptor
+        // for (let object of objects) {
+        //
+        //     // @ts-ignore
+        //     for (let prop in object.prototype) {
+        //         // @ts-ignore
+        //         target.prototype[prop] = object.prototype[prop];
+        //     }
+        //
+        // }
+        // target.prototype()
+        //console.log(descriptors);
         // assign descriptor
         for (let [property, descriptor] of descriptors) {
-            Object.defineProperty(target, property, descriptor);
+            Object.prototype[property] = descriptor;
         }
-        return target;
+        return Object.assign(new target, ...objects);
     }
     exports.default = Mixin;
 });
--- spec/mixin.spec.ts
+++ spec/mixin.spec.ts
@@ -81,250 +81,319 @@ class Property {
 
 }
 
-
-describe('accessor', () => {
-
-    let accessor = new Accessor(10);
-    let mixin = Mixin(accessor);
-
-    it('default', () => {
-
-        expect(mixin.value).toBe(10);
-
-    });
-
-});
-
-describe('single', () => {
-
-    let accessor = new Mutator(10);
-    let mixin = Mixin(accessor);
-
-    it('default', () => {
-
-        expect(mixin.value).toBeUndefined();
-
-    });
-});
-
-
-describe('data', () => {
-
-    let method = new Method('a');
-    let mixin = Mixin(method);
-
-    it('default', () => {
-
-        expect(typeof mixin.getData).toBe('function');
-        expect(typeof mixin.setData).toBe('function');
-        expect(mixin.getData()).toBe('a');
-
-    });
-
-    it('mutated', () => {
-
-        mixin.setData('c');
-        expect(mixin.getData()).toBe('c');
-
-    });
-
-    it('original', () => {
-
-        expect(typeof method.getData).toBe('function');
-        expect(typeof method.setData).toBe('function');
-        expect(method.getData()).toBe('a');
-    });
-
-});
-
-
-
-describe('symbol', () => {
-
-    let data = ['a', 'b'];
-    let symbol = new Symbol_(data);
-    let mixin = Mixin(symbol);
-
-    it('default', () => {
-
-        let i = 0;
-        for (let val of mixin) {
-            expect(val).toBe(data[i]);
-            i++;
-        }
-
-    });
-
-    it('mutated', () => {
-
-        let data2 = ['aa', 'bb', 'cc'];
-        mixin.iterable = data2;
-
-        let j = 0;
-        for (let val of mixin) {
-            expect(val).toBe(data2[j], `mixin[Symbol.iterator] ${j}`);
-            j++;
-        }
-
-    });
-
-    it('original', () => {
-
-        let i = 0;
-        for (let val of symbol) {
-            expect(val).toBe(data[i]);
-            i++;
-        }
-
-    });
-});
-
-
-
-describe('property', () => {
-
-    let property = new Property('string');
-    let mixin = Mixin(property);
-
-    it('default', () => {
-
-        expect(mixin.data).toBe('string');
-
-    });
-
-    it('mutated', () => {
-
-        mixin.data = 'string2';
-        expect(mixin.data).toBe('string2');
-
-    });
-
-    it('original', () => {
-
-        expect(property.data).toBe('string');
-    });
-});
-
-
-describe('accessor & mutator', () => {
-
-    let accessor = new Accessor(1);
-    let mutator = new Mutator(2);
-    let mixin = <Mutable<Accessor>> Mixin(accessor, mutator);
-
-    it('default', () => {
-
-        expect(mixin.value).toBe(2, 'mixin');
-
-    });
-
-    it('mutated', () => {
-
-        mixin.value = 10;
-        expect(mixin.value).toBe(10, 'mixin after set');
-    });
-
-    it('original', () => {
-
-        expect(accessor.value).toBe(1, 'accessor');
-        expect(mutator.value).toBeUndefined();
-
-    });
-
-});
-
-describe('method, accessor & mutator', () => {
-
-    let method = new Method('str');
-    let accessor = new Accessor(1);
-    let mutator = new Mutator(2);
-    let mixin = <Mutable<Accessor> & Mutator & Method> Mixin(accessor, mutator, method);
-
-    it('default', () => {
-
-        expect(mixin.getData()).toBe('str', 'mixin.getData default');
-        expect(mixin.value).toBe(2, 'mixin.value default');
-    });
-
-    it('mutated', () => {
-
-        mixin.value = 10;
-        mixin.setData('data');
-        expect(mixin.getData()).toBe('data', 'set mixin.getData');
-        expect(mixin.value).toBe(10, 'set mixin.value');
-
-    });
-
-    it('original', () => {
-
-        expect(accessor.value).toBe(1, 'method.getData default');
-        expect(method.getData()).toBe('str', 'accessor.value default');
-        expect(mutator.value).toBeUndefined();
-
-    });
-});
-
-describe('symbol, accessor & mutator', () => {
-
-    let data = ['a', 'b'];
-
-    let method = new Symbol_(data);
-    let accessor = new Accessor(1);
-    let mutator = new Mutator(2);
-    let mixin = <Mutable<Accessor> & Mutator & Symbol_> Mixin(accessor, mutator, method);
-
-    it('default', () => {
-
-        expect(mixin.value).toBe(2, 'mixin.value');
-
-        let i = 0;
-        for (let val of mixin) {
-            expect(val).toBe(data[i], `mixin[Symbol.iterator] ${i}`);
-            i++;
-        }
-    });
-
-    it('mutated', () => {
-
-        mixin.value = 10;
-        let data2 = ['aa', 'bb', 'cc'];
-        mixin.iterable = data2;
-
-        expect(mixin.value).toBe(10, 'mixin.value');
-
-        let j = 0;
-        for (let val of mixin) {
-            expect(val).toBe(data2[j], `mixin[Symbol.iterator] ${j}`);
-            j++;
-        }
-    });
-
-    it('original', () => {
-
-        let k = 0;
-        for (let val of method) {
-            expect(val).toBe(data[k], `mixin[Symbol.iterator] ${k}`);
-            k++;
-        }
-
-        expect(accessor.value).toBe(1, 'method.getData');
-
-        expect(mutator.value).toBeUndefined();
-    });
-
-});
-
-
-describe('symbol, method, property, accessor & mutator', () => {
+//
+// describe('accessor', () => {
+//
+//     let accessor = new Accessor(10);
+//     let mixin = Mixin(accessor);
+//
+//     it('default', () => {
+//
+//         expect(mixin.value).toBe(10);
+//
+//     });
+//
+// });
+//
+// describe('single', () => {
+//
+//     let accessor = new Mutator(10);
+//     let mixin = Mixin(accessor);
+//
+//     it('default', () => {
+//
+//         expect(mixin.value).toBeUndefined();
+//
+//     });
+// });
+//
+//
+// describe('data', () => {
+//
+//     let method = new Method('a');
+//     let mixin = Mixin(method);
+//
+//     it('default', () => {
+//
+//         expect(typeof mixin.getData).toBe('function');
+//         expect(typeof mixin.setData).toBe('function');
+//         expect(mixin.getData()).toBe('a');
+//
+//     });
+//
+//     it('mutated', () => {
+//
+//         mixin.setData('c');
+//         expect(mixin.getData()).toBe('c');
+//
+//     });
+//
+//     it('original', () => {
+//
+//         expect(typeof method.getData).toBe('function');
+//         expect(typeof method.setData).toBe('function');
+//         expect(method.getData()).toBe('a');
+//     });
+//
+// });
+//
+//
+//
+// describe('symbol', () => {
+//
+//     let data = ['a', 'b'];
+//     let symbol = new Symbol_(data);
+//     let mixin = Mixin(symbol);
+//
+//     it('default', () => {
+//
+//         let i = 0;
+//         for (let val of mixin) {
+//             expect(val).toBe(data[i]);
+//             i++;
+//         }
+//
+//     });
+//
+//     it('mutated', () => {
+//
+//         let data2 = ['aa', 'bb', 'cc'];
+//         mixin.iterable = data2;
+//
+//         let j = 0;
+//         for (let val of mixin) {
+//             expect(val).toBe(data2[j], `mixin[Symbol.iterator] ${j}`);
+//             j++;
+//         }
+//
+//     });
+//
+//     it('original', () => {
+//
+//         let i = 0;
+//         for (let val of symbol) {
+//             expect(val).toBe(data[i]);
+//             i++;
+//         }
+//
+//     });
+// });
+//
+//
+//
+// describe('property', () => {
+//
+//     let property = new Property('string');
+//     let mixin = Mixin(property);
+//
+//     it('default', () => {
+//
+//         expect(mixin.data).toBe('string');
+//
+//     });
+//
+//     it('mutated', () => {
+//
+//         mixin.data = 'string2';
+//         expect(mixin.data).toBe('string2');
+//
+//     });
+//
+//     it('original', () => {
+//
+//         expect(property.data).toBe('string');
+//     });
+// });
+//
+//
+// describe('accessor & mutator', () => {
+//
+//     let accessor = new Accessor(1);
+//     let mutator = new Mutator(2);
+//     let mixin = <Mutable<Accessor>> Mixin(accessor, mutator);
+//
+//     it('default', () => {
+//
+//         expect(mixin.value).toBe(2, 'mixin');
+//
+//     });
+//
+//     it('mutated', () => {
+//
+//         mixin.value = 10;
+//         expect(mixin.value).toBe(10, 'mixin after set');
+//     });
+//
+//     it('original', () => {
+//
+//         expect(accessor.value).toBe(1, 'accessor');
+//         expect(mutator.value).toBeUndefined();
+//
+//     });
+//
+// });
+//
+// describe('method, accessor & mutator', () => {
+//
+//     let method = new Method('str');
+//     let accessor = new Accessor(1);
+//     let mutator = new Mutator(2);
+//     let mixin = <Mutable<Accessor> & Mutator & Method> Mixin(accessor, mutator, method);
+//
+//     it('default', () => {
+//
+//         expect(mixin.getData()).toBe('str', 'mixin.getData default');
+//         expect(mixin.value).toBe(2, 'mixin.value default');
+//     });
+//
+//     it('mutated', () => {
+//
+//         mixin.value = 10;
+//         mixin.setData('data');
+//         expect(mixin.getData()).toBe('data', 'set mixin.getData');
+//         expect(mixin.value).toBe(10, 'set mixin.value');
+//
+//     });
+//
+//     it('original', () => {
+//
+//         expect(accessor.value).toBe(1, 'method.getData default');
+//         expect(method.getData()).toBe('str', 'accessor.value default');
+//         expect(mutator.value).toBeUndefined();
+//
+//     });
+// });
+//
+// describe('symbol, accessor & mutator', () => {
+//
+//     let data = ['a', 'b'];
+//
+//     let method = new Symbol_(data);
+//     let accessor = new Accessor(1);
+//     let mutator = new Mutator(2);
+//     let mixin = <Mutable<Accessor> & Mutator & Symbol_> Mixin(accessor, mutator, method);
+//
+//     it('default', () => {
+//
+//         expect(mixin.value).toBe(2, 'mixin.value');
+//
+//         let i = 0;
+//         for (let val of mixin) {
+//             expect(val).toBe(data[i], `mixin[Symbol.iterator] ${i}`);
+//             i++;
+//         }
+//     });
+//
+//     it('mutated', () => {
+//
+//         mixin.value = 10;
+//         let data2 = ['aa', 'bb', 'cc'];
+//         mixin.iterable = data2;
+//
+//         expect(mixin.value).toBe(10, 'mixin.value');
+//
+//         let j = 0;
+//         for (let val of mixin) {
+//             expect(val).toBe(data2[j], `mixin[Symbol.iterator] ${j}`);
+//             j++;
+//         }
+//     });
+//
+//     it('original', () => {
+//
+//         let k = 0;
+//         for (let val of method) {
+//             expect(val).toBe(data[k], `mixin[Symbol.iterator] ${k}`);
+//             k++;
+//         }
+//
+//         expect(accessor.value).toBe(1, 'method.getData');
+//
+//         expect(mutator.value).toBeUndefined();
+//     });
+//
+// });
+//
+//
+// describe('symbol, method, property, accessor & mutator', () => {
+//
+//     let data = ['a', 'b'];
+//
+//     let symbol = new Symbol_(data);
+//     let method = new Method('method');
+//     let property = new Property('property');
+//     let accessor = new Accessor(1);
+//     let mutator = new Mutator(2);
+//     let mixin = <Mutable<Accessor> & Mutator & Symbol_ & Mutable<Method> & Property> Mixin(accessor, mutator, symbol, method, property);
+//
+//     it('default', () => {
+//
+//         expect(mixin.value).toBe(2, 'mixin.value');
+//         expect(mixin.getData()).toBe('property', 'method.getData');
+//         expect(mixin.data).toBe('property', 'method.getData');
+//
+//         let i = 0;
+//         for (let val of mixin) {
+//             expect(val).toBe(data[i], `mixin[Symbol.iterator] ${i}`);
+//             i++;
+//         }
+//     });
+//
+//     it('mutated', () => {
+//
+//         mixin.setData('data');
+//         mixin.value = 10;
+//         let data2 = ['aa', 'bb', 'cc'];
+//         mixin.iterable = data2;
+//
+//         expect(mixin.value).toBe(10, 'mixin.value');
+//         expect(mixin.getData()).toBe('data', 'mixin.getData');
+//         expect(mixin.data).toBe('data', 'mixin.data');
+//
+//         let j = 0;
+//         for (let val of mixin) {
+//             expect(val).toBe(data2[j], `mixin[Symbol.iterator] ${j}`);
+//             j++;
+//         }
+//     });
+//
+//     it('original', () => {
+//
+//         let k = 0;
+//         for (let val of symbol) {
+//             expect(val).toBe(data[k], `mixin[Symbol.iterator] ${k}`);
+//             k++;
+//         }
+//
+//         expect(accessor.value).toBe(1, 'method.getData');
+//         expect(property.data).toBe('property', 'property.data');
+//         expect(method.getData()).toBe('method', 'method.getData');
+//
+//         expect(mutator.value).toBeUndefined();
+//     });
+//
+// });
+
+
+describe('chain (symbol, method), (property, accessor), mutator', () => {
 
     let data = ['a', 'b'];
 
     let symbol = new Symbol_(data);
     let method = new Method('method');
+    let mixin1 = <Symbol_ & Mutable<Method>> Mixin(symbol, method);
+
     let property = new Property('property');
     let accessor = new Accessor(1);
+    let mixin2 = <Mutable<Accessor> &  Symbol_ & Mutable<Method> & Property> Mixin(accessor, mixin1, property);
+
     let mutator = new Mutator(2);
-    let mixin = <Mutable<Accessor> & Mutator & Symbol_ & Mutable<Method> & Property> Mixin(accessor, mutator, symbol, method, property);
+    let mixin = <Mutable<Accessor> & Mutator & Symbol_ & Mutable<Method> & Property> Mixin(mutator, mixin2);
+
+    // @ts-ignore
+    console.log(Object.getPrototypeOf(mixin));
+    console.log(mixin.getData());
+    console.log(mixin.setData);
 
     it('default', () => {
 
