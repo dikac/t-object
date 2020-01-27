2466ca325e7b986eeee2850baa92976a3d64597b
dikac <rigel234@yahoo.com>
2020-01-28 05:55:42 +0700

    make StrictAssign second generic parameter optional

--- dist/strict-assign.d.ts
+++ dist/strict-assign.d.ts
@@ -5,4 +5,4 @@
  * @param data
  * @constructor
  */
-export default function StrictAssign<O extends object, U extends Partial<O>>(object: O, data: U): U & Omit<O, keyof U>;
+export default function StrictAssign<O extends object, U extends Partial<O> = Partial<O>>(object: O, data: U): U & Omit<O, keyof U>;
--- src/strict-assign.ts
+++ src/strict-assign.ts
@@ -5,7 +5,7 @@
  * @param data
  * @constructor
  */
-export default function StrictAssign<O extends object, U extends Partial<O>>(object: O, data: U): U & Omit<O, keyof U>{
+export default function StrictAssign<O extends object, U extends Partial<O> = Partial<O>>(object: O, data: U): U & Omit<O, keyof U>{
 
     return Object.assign(object, data);
 }
