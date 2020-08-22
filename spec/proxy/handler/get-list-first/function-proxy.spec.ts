fc808ef1897bda1332138dc73578273f2490c71d
dikac <rigel234@yahoo.com>
2020-08-22 17:15:54 +0700

    improve spec

---
+++ spec/proxy/handler/get-list-first/function-proxy.spec.ts
@@ -0,0 +1,32 @@
+import GetHandler from "../../../../dist/proxy/handler/get-list-first";
+import GetOwnPropertyDescriptorListAll from "../../../../dist/proxy/handler/get-own-property-descriptor-list-all";
+import MergeAnonymous from "../../../../dist/proxy/handler/merge-anonymous";
+import GetPrototypeOfListMerge from "../../../../dist/proxy/handler/prototype-of-list-merge";
+
+
+it("enable console log", () => { spyOn(console, 'log').and.callThrough();});
+
+
+class Property {
+
+    constructor(
+        public data : string = 'property'
+    ) {
+    }
+}
+
+let property1 = new Property('property 1');
+
+describe('direct set', () => {
+
+    let getter = new GetHandler([property1]);
+    let proxy = <Property & Function>new Proxy(function (value : number){ return value + value;}, getter)
+
+    it('check value', ()=>{
+        expect(proxy.data).toBe('property 1');
+    });
+
+    it('check callback', ()=>{
+        expect(proxy(5)).toBe(10);
+    });
+});
